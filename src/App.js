83db25ecfca41b856cf5bc183ec8ee9b30e4b8af
Zainul Franciscus <Zainul.Franciscus@myob.com>
2021-04-14 15:30:27 +1000

    migrate to vanilla extract

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 // import "./App.css";
 
-import InternalTreatButton from "./components/InternalTreatButton";
+import InternalTreatButton from "./components/InternalTreatButton/InternalTreatButton";
 
 function App() {
   return (
